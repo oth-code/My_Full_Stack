e55b1374a5fecf3cc42534f4fbf76e98b0c1ab04
Othmane Ouajjou <ouajjouothmane@gmail.com>
2025-09-14 20:42:12 -0400

    sauvegarde temporaire avant sync

--- api/index.js
+++ api/index.js
@@ -1,13 +1,4 @@
 import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-dotenv.config();
-
-mongoose.connect(process.env.MONGO_URI, ).then(() => {
-    console.log('Connected to MongoDB');
-}).catch((error) => {
-    console.error('Error connecting to MongoDB:', error);
-});
 
 const app = express();
 
